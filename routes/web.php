aee8337b01a40c3924e09a9b99713fef7a18ca2a
bwf1dodolo <82048279+bwf1dodolo@users.noreply.github.com>
2022-09-21 14:01:48 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,20 @@ use App\Http\Controllers\DownloadController;
 */
 
 Route::get('first_page', function () {
-    return view('first_page');
+//     return view('first_page');
+    return [
+        'payment Due',
+        'Be Professional'
+    ]
 });
-Route::get('second_page', function () {
-    return view('second_page');
-})->name('second_page');
+// Route::get('second_page', function () {
+//     return view('second_page');
+// })->name('second_page');
 // Route::get('third_page', function () {
 //     return view('third_page');
 // })->name('third_page');
 
-Route::post('authentication',[AuthenticationController::class,'authentication'])->name('authentication');
-Route::post('third_page',[DownloadController::class,'thirdpage'])->name('post_third_page');
-Route::post('download',[DownloadController::class,'download'])->name('download');
+// Route::post('authentication',[AuthenticationController::class,'authentication'])->name('authentication');
+// Route::post('third_page',[DownloadController::class,'thirdpage'])->name('post_third_page');
+// Route::post('download',[DownloadController::class,'download'])->name('download');
 
